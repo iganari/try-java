c61b81f525fc2de92d7780d4be857e58c8b8be75
iganari <cigaguri@gmail.com>
2016-12-08 21:00:57 +0900

    1-05追加

---
+++ kita_soft_koubo/1-05/Multiplication.java
@@ -0,0 +1,10 @@
+public class Multiplication {
+  public static void main(String args[]){
+    int x = 13;
+    int y = 17;
+    int z     ;
+
+    System.out.println("x * y = " + ( x * y ) + " です。" );
+
+  }
+}
