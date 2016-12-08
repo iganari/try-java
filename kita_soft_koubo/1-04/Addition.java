7b2a79b1311ebcbe40d7029167185fe68eb91007
iganari <cigaguri@gmail.com>
2016-12-08 12:21:33 +0900

    1-04 OK

---
+++ kita_soft_koubo/1-04/Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+  public static void main(String args[]){
+    int z = 13;
+    int y = 17;
+    int x     ;
+
+    System.out.println("zは" + z + "です。");
+    System.out.println("yは" + y + "です。");
+    System.out.println("xは( z + y ) = " + ( z + y ) + "です");
+
+  }
+}
